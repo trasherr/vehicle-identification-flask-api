7e68321b8725160226a5dace2c143e3c2ddbe1a1
Samratrpal01 <Samratrpal01@gmail.com>
2023-10-06 10:51:02 +0530

    Created TAble and Added Value to it

--- app.py
+++ app.py
@@ -4,40 +4,57 @@ from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 
-
-CREATE_ROOMS_TABLE=(
-    "CREATE TABLE IF NOT EXISTS Vehicle_Record(id SERIAL PRIMARY KEY,carNo VARCHAR(10),inTime TIMESTAMP,outTime TIMESTAMP)"
+CREATE_VEHICLE_TABLE =(
+    "CREATE TABLE IF NOT EXISTS Vehicle_Record ( id SERIAL PRIMARY KEY,carNo VARCHAR(10), inTime TIMESTAMP,outTime TIMESTAMP);"   
+    
 )
 
+INSERT_VEHICLE = "INSERT INTO Vehicle_Record (carNo, inTime, outTime) VALUES (%s, %s, %s);"
+
 load_dotenv()
-# creating the flask app
+
+# Creating the flask app
 app = Flask(__name__)
-url=os.getenv("DATABASE_URL")
+url = os.getenv("DATABASE_URL")
 api = Api(app)
 
-connection=psycopg2.connect(url)
-class Hello(Resource):
+# Establishing a database connection
+connection = psycopg2.connect(url)
 
+
+class Hello(Resource):
     def get(self):
-  
-        return jsonify({'message': 'hello world'})
-  
+        return jsonify({'message': 'Hello, world'})
+
     def post(self):
         data = request.get_json()
         with connection.cursor() as cursor:
-            cursor.execute(CREATE_ROOMS_TABLE) 
+            cursor.execute(CREATE_VEHICLE_TABLE)
         return jsonify({'data': data}), 201
 
-  
+
 api.add_resource(Hello, '/')
-#api.add_resource(Upload,'/upload')
-@app.post('/upload')
-def create_room():
-    data=request.get_json()
-    name=data["name"]
-    with connection.cursor() as cursor:
-        cursor.execute(CREATE_ROOMS_TABLE)
-    return {"message":"Yes Table Created"}
-  
+
+# API endpoint for uploading data
+@app.route('/upload', methods=['POST'])
+def upload_data():
+    try:
+        data = request.get_json()
+        car_no = data["carNo"]
+        in_time = data["inTime"]
+        out_time = data["outTime"]
+
+        with connection.cursor() as cursor:
+            cursor.execute(CREATE_VEHICLE_TABLE)
+            cursor.execute(INSERT_VEHICLE, (car_no, in_time, out_time))
+        
+        connection.commit()
+
+        return {"message": "Table created and data inserted successfully"}
+
+    except Exception as e:
+        return {"error": str(e)}
+
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
