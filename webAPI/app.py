4841a46fba636e517aed323b00a83e47d71c5c62
rahulpal <rahul.pal@nagarro.com>
2023-10-18 14:43:24 +0530

    adding upload service

--- webAPI/app.py
+++ webAPI/app.py
@@ -3,23 +3,13 @@ import psycopg2
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
-from io import BytesIO
 
-CREATE_VEHICLE_TABLE = (
-    "CREATE TABLE IF NOT EXISTS Vehicle_Record ("
-    "id SERIAL PRIMARY KEY, "
-    "carNo VARCHAR(10), "
-    "inTime TIMESTAMP, "
-    "outTime TIMESTAMP, "
-    "video BYTEA);"
+CREATE_VEHICLE_TABLE =(
+    "CREATE TABLE IF NOT EXISTS Vehicle_Record ( id SERIAL PRIMARY KEY,carNo VARCHAR(10), inTime TIMESTAMP,outTime TIMESTAMP);"   
+    
 )
 
-
-
-
-
-
-INSERT_VEHICLE = "INSERT INTO Vehicle_Record (carNo, inTime, outTime, video) VALUES (%s, %s, %s, %s);"
+INSERT_VEHICLE = "INSERT INTO Vehicle_Record (carNo, inTime, outTime) VALUES (%s, %s, %s);"
 
 load_dotenv()
 
@@ -41,31 +31,46 @@ class Hello(Resource):
         with connection.cursor() as cursor:
             cursor.execute(CREATE_VEHICLE_TABLE)
         return jsonify({'data': data}), 201
-
+    
+   
 
 api.add_resource(Hello, '/')
 
+# API endpoint for uploading data
 
-# API endpoint for uploading data and video file
+@app.route('/video',methods=['POST'])
+def upload_video():
+    try:
+        
+        video_file = request.files['video']
+        
+       
+
+        print(video_file)
+        video_file.save('video.mp4')
+        
+        return {"message": "Video uploaded successfully"}
+
+    except Exception as e:
+        return {"error": str(e)}
+
+
+
 @app.route('/upload', methods=['POST'])
 def upload_data():
     try:
-        data = request.form.to_dict()
+        data = request.get_json()
         car_no = data["carNo"]
         in_time = data["inTime"]
         out_time = data["outTime"]
 
-        # Get the uploaded file
-        file = request.files['video']
-        file_data = BytesIO(file.read())
-
         with connection.cursor() as cursor:
             cursor.execute(CREATE_VEHICLE_TABLE)
-            cursor.execute(INSERT_VEHICLE, (car_no, in_time, out_time, file_data.read()))
+            cursor.execute(INSERT_VEHICLE, (car_no, in_time, out_time))
         
         connection.commit()
 
-        return {"message": "Table created, data inserted, and video file uploaded successfully"}
+        return {"message": "Table created and data inserted successfully"}
 
     except Exception as e:
         return {"error": str(e)}
