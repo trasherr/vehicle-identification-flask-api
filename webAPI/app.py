27f9c532cadaced53187f8f2d0904c5a9795431d
rahulpal <rahul.pal@nagarro.com>
2023-10-18 14:04:37 +0530

    updating changes

--- webAPI/app.py
+++ webAPI/app.py
@@ -3,13 +3,23 @@ import psycopg2
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+from io import BytesIO
 
-CREATE_VEHICLE_TABLE =(
-    "CREATE TABLE IF NOT EXISTS Vehicle_Record ( id SERIAL PRIMARY KEY,carNo VARCHAR(10), inTime TIMESTAMP,outTime TIMESTAMP);"   
-    
+CREATE_VEHICLE_TABLE = (
+    "CREATE TABLE IF NOT EXISTS Vehicle_Record ("
+    "id SERIAL PRIMARY KEY, "
+    "carNo VARCHAR(10), "
+    "inTime TIMESTAMP, "
+    "outTime TIMESTAMP, "
+    "video BYTEA);"
 )
 
-INSERT_VEHICLE = "INSERT INTO Vehicle_Record (carNo, inTime, outTime) VALUES (%s, %s, %s);"
+
+
+
+
+
+INSERT_VEHICLE = "INSERT INTO Vehicle_Record (carNo, inTime, outTime, video) VALUES (%s, %s, %s, %s);"
 
 load_dotenv()
 
@@ -35,22 +45,27 @@ class Hello(Resource):
 
 api.add_resource(Hello, '/')
 
-# API endpoint for uploading data
+
+# API endpoint for uploading data and video file
 @app.route('/upload', methods=['POST'])
 def upload_data():
     try:
-        data = request.get_json()
+        data = request.form.to_dict()
         car_no = data["carNo"]
         in_time = data["inTime"]
         out_time = data["outTime"]
 
+        # Get the uploaded file
+        file = request.files['video']
+        file_data = BytesIO(file.read())
+
         with connection.cursor() as cursor:
             cursor.execute(CREATE_VEHICLE_TABLE)
-            cursor.execute(INSERT_VEHICLE, (car_no, in_time, out_time))
+            cursor.execute(INSERT_VEHICLE, (car_no, in_time, out_time, file_data.read()))
         
         connection.commit()
 
-        return {"message": "Table created and data inserted successfully"}
+        return {"message": "Table created, data inserted, and video file uploaded successfully"}
 
     except Exception as e:
         return {"error": str(e)}
